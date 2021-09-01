5aea772c2251e8e24a53877928cf19d86141b933
akon47 <akon47@naver.com>
2021-09-01 14:56:44 +0900

    초기 커밋.

---
+++ AvoidingPoo/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// AvoidingPoo.rc에서 사용되고 있습니다.
+//
+#define IDB_BITMAP1                     102
+#define IDB_BITMAP2                     103
+#define IDB_BITMAP3                     104
+#define IDB_BITMAP4                     105
+#define IDB_BITMAP5                     106
+#define IDB_BITMAP6                     107
+#define IDB_BITMAP7                     108
+#define IDB_BITMAP8                     109
+#define IDB_BITMAP9                     110
+#define IDB_BITMAP10                    111
+#define IDB_BITMAP11                    112
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
